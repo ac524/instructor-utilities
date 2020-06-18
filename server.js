1beda72854030504488f325058fd4329d4627d60
Anthony Brown <ac524.brown@gmail.com>
2020-06-17 20:42:11 -0700

    Added needed configuration for the Heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = 3000;
+const PORT = process.env.PORT ||3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
