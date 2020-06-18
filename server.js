40b26d178773c781145c0da5bae9f4402f1daccf
Christopher Marti <chrisjm093@gmail.com>
2020-06-17 20:13:12 -0700

    Static route configured, routes folder and htmlRoutes.js added

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use( express.static( 'public' ));
+
+
+
 // Dummy route
 app.get("/", (req, res) => {
     res.end("Successful connection!");
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log("App listening on Port" + PORT);
