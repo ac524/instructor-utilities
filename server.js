a777778bb8f451518cbefd8814f2edb10cbfc9a3
Tcreamer <trentoncreamer@yahoo.com>
2020-06-17 19:57:29 -0700

    commiting to pull

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// dummy test connection 
+(req, res) => {
+  res.end("success!");
+  // res.sendFile(path.join(__dirname, ""));
+});
+
 app.listen(PORT, () => {
   console.log("App listening on Port" + PORT);
 });
