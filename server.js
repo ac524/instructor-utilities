703eb90a2e557930e2461743f4e6438953dada3b
Christopher Marti <chrisjm093@gmail.com>
2020-06-17 20:30:04 -0700

    added htmlRoutes to server.js file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const htmlRoutes = require('/routes/htmlRoutes');
 
 const PORT = 3000;
 
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use( express.static( 'public' ));
-
+app.use( htmlRoutes );
 
 
 // Dummy route
