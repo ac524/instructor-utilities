96e08b02abd21bbc6106100c5cadf969cb864a46
Anthony Brown <ac524.brown@gmail.com>
2020-07-12 23:16:26 -0500

    Moved the lists tool to /lists. Added middleware to redirect logged in users to protect routes that should only be accessed if not logged in

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,15 +1,22 @@
+const redirectAuthenticated = require("../config/middleware/redirectAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
 const router = require("express").Router();
 
-router.get("/signup", (req, res) =>{
+router.get("/signup", redirectAuthenticated, (req, res) =>{
   res.render( "signup" );
 });
 
-router.get("/", (req, res) => {
+router.get("/", redirectAuthenticated, (req, res) => {
+  res.render( "home" );
+});
+
+router.get("/lists", isAuthenticated, (req, res) => {
   res.render( "lists" );
 });
 
 router.get("*", (req, res) => {
-  res.render( "lists" );
+  res.redirect( "/" );
 });
 
 
