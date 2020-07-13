36bb12961f51027d1ac772eb2445f240757a73fa
Anthony Brown <ac524.brown@gmail.com>
2020-07-13 11:49:48 -0500

    Added starting file for api interaction

--- models/ListItem.js
+++ models/ListItem.js
@@ -4,12 +4,10 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false
         },
-
         selected: {
             type: DataTypes.BOOLEAN,
             defaultValue: 0
         },
-
         disabled: {
             type: DataTypes.BOOLEAN,
             defaultValue: 0
---
+++ resources/js/api.js
@@ -0,0 +1,43 @@
+class Api {
+
+    createList( listData ) {
+        
+        // Execute and return an ajax call to create a new list
+
+    }
+
+    updateList( listId, listData ) {
+
+        // Execute and return an ajax call to upate a list
+
+    }
+
+    deleteList( listId ) {
+
+        // Execute and return an ajax call to delete a list
+
+    }
+
+    createListItem( listItemData ) {
+        
+        // Execute and return an ajax call to create a new list item
+
+    }
+
+    updateListItem() {
+        
+        // Execute and return an ajax call to create a new list item
+
+    }
+
+    deleteListItem() {
+        
+        // Execute and return an ajax call to delete a new list item
+        
+    }
+
+}
+
+const api = new Api();
+
+export default api;
