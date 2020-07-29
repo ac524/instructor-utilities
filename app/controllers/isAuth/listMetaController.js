beea0baed5984565665eb8c9f18d5408dce21c19
Anthony Brown <ac524.brown@gmail.com>
2020-07-29 10:28:36 -0500

    Removed old code that was commented out

--- app/controllers/isAuth/listMetaController.js
+++ app/controllers/isAuth/listMetaController.js
@@ -14,21 +14,4 @@ metaIdArrays.forEach( metaKey => {
 
 } );
 
-
-
-// router.post( "/lists/:listId/select", async ( req, res ) => await pushToValueArray( req, res, 'selected' ) );
-
-// router.patch( "/lists/:listId/unselect", async ( req, res ) => await removeFromValueArray( req, res, 'selected' ));
-
-// router.patch( "/lists/:listId/select/clear", async ( req, res ) => await clearValueArray( req, res, 'selected' ));
-
-// router.post( "/lists/:listId/disable", async ( req, res ) => await pushToValueArray( req, res, 'disabled' ) );
-
-// router.patch( "/lists/:listId/enable", async ( req, res ) => await removeFromValueArray( req, res, 'disabled' ));
-
-// router.patch( "/lists/:listId/disable/clear", async ( req, res ) => await clearValueArray( req, res, 'disabled' ));
-
-
-// router.post( "/lists/:listId/disabled", async ( req, res ) => await pushToValueArray( req, res, 'disabled' ) );
-
 module.exports = router;
