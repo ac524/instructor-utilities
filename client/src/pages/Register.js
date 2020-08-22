de52ff48b7c2b191b842468a167ee232219459a8
Anthony Brown <ac524.brown@gmail.com>
2020-08-22 16:46:25 -0700

    Refactored registration form to use new Form component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 
 import Hero from 'react-bulma-components/lib/components/hero';
 import Container from 'react-bulma-components/lib/components/container';
-import { Field, Control, Label, Input } from 'react-bulma-components/lib/components/form';
 import Columns from "react-bulma-components/lib/components/columns";
 import Box from "react-bulma-components/lib/components/box";
-import Button from "react-bulma-components/lib/components/button";
 import Heading from 'react-bulma-components/lib/components/heading';
 
 import { LoginLink } from "../components/Login";
 
 import api from "../utils/api";
 import { useLogin } from "../utils/auth";
+import Form from "../components/Form";
 
 const { Column } = Columns;
 
@@ -23,17 +22,56 @@ function Register() {
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
     const [ password2, setPassword2 ] = useState("");
+    const [ errors, setErrors ] = useState({});
+
+    const fields = [
+        {
+            label: "Username",
+            onChange: (e) => setName(e.target.value),
+            value: name,
+            name: "name"
+        },
+        {
+            label: "Email",
+            onChange: (e) => setEmail(e.target.value),
+            value: email,
+            type:"email",
+            name: "email"
+        },
+        {
+            label: "Password",
+            onChange: (e) => setPassword(e.target.value),
+            value: password,
+            type:"password",
+            name: "password"
+        },
+        {
+            label: "Confirm Password",
+            onChange: (e) => setPassword2(e.target.value),
+            value: password2,
+            type:"password",
+            name: "password2"
+        }
+    ];
 
     const handleSubmit = async (e) => {
 
         e.preventDefault();
 
-        await api.register({ name, email, password, password2 });
+        try {
 
-        // Auto Login after registration
-        await login( { email, password } );
+            await api.register({ name, email, password, password2 });
 
-        window.location.href = "./";
+            // Auto Login after registration
+            await login( { email, password } );
+    
+            window.location.href = "./";
+
+        } catch( err ) {
+
+            if( err.response.data ) setErrors( err.response.data );
+
+        }
 
     }
 
@@ -46,34 +84,8 @@ function Register() {
                             <Box>
                             <Heading renderAs="h2" className="has-text-dark">Register</Heading>
                             <hr />
-                            <form onSubmit={handleSubmit}>
-                                <Field>
-                                    <Label>Username</Label>
-                                    <Control>
-                                        <Input onChange={(e) => setName(e.target.value)} value={name} />
-                                    </Control>
-                                </Field>
-                                <Field>
-                                    <Label>Email</Label>
-                                    <Control>
-                                        <Input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
-                                    </Control>
-                                </Field>
-                                <Field>
-                                    <Label>Password</Label>
-                                    <Control>
-                                        <Input onChange={(e) => setPassword(e.target.value)} value={password} />
-                                    </Control>
-                                </Field>
-                                <Field>
-                                    <Label>Repeat Password</Label>
-                                    <Control>
-                                        <Input onChange={(e) => setPassword2(e.target.value)} value={password2} />
-                                    </Control>
-                                </Field>
-                                <Button color="primary mt-4" fullwidth>Signup</Button>
-                                <p className="mt-3 has-text-grey is-size-7">Already have an account? <LoginLink /></p>
-                            </form>
+                            <Form fields={fields} errors={errors} buttonText="Sign Up" onSubmit={handleSubmit} />
+                            <p className="mt-3 has-text-grey is-size-7">Already have an account? <LoginLink /></p>
                             </Box>
                         </Column>
                     </Columns>
