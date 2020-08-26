2fa38767681abb6a09c33ec542b59a9e42f0141b
Anthony Brown <ac524.brown@gmail.com>
2020-08-26 10:56:43 -0700

    Moved main <Router> component to global usage to ensure 'history' is always available through the 'useHistory' hook.

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Pages from "../pages";
 
 import { useIsAuthenticated } from "../utils/auth";
@@ -23,15 +23,13 @@ function Routes() {
     const isAuth = useIsAuthenticated();
 
     return (
-        <Router>
-            <Switch>
-                <Route path="/devs" exact component={Pages.Developers} />
-                <Route path="/privacy" exact component={Pages.Privacy} />
-                <GuestRoute path="/register" exact><Pages.Register /></GuestRoute>
-                <Route path="/" exact={!isAuth} component={isAuth ? Pages.Dashboard : Pages.Home} />
-                <Route path="*" component={Pages.Developers} />
-            </Switch>
-        </Router>
+        <Switch>
+            <Route path="/devs" exact component={Pages.Developers} />
+            <Route path="/privacy" exact component={Pages.Privacy} />
+            <GuestRoute path="/register" exact><Pages.Register /></GuestRoute>
+            <Route path="/" exact={!isAuth} component={isAuth ? Pages.Dashboard : Pages.Home} />
+            <Route path="*" component={Pages.Developers} />
+        </Switch>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { StoreProvider } from "./store";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
         <StoreProvider>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </StoreProvider>
     </React.StrictMode>,
     document.getElementById('root')
