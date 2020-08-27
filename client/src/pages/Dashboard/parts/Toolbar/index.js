319d6068f543340a74022158c0bf2363b65d01e1
Anthony Brown <ac524.brown@gmail.com>
2020-08-26 20:54:54 -0700

    Abstracted template for toolbar items to own component. Set up data array for configuring toolbar links.

--- client/src/pages/Dashboard/parts/Toolbar/index.js
+++ client/src/pages/Dashboard/parts/Toolbar/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 import "./style.sass";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LogoutLink } from "../../../../components/Login";
 
@@ -17,27 +17,35 @@ export const useToolbarOffset = ( offset ) => {
 
 }
 
+const ToolbarItem = ({ icon, ...props }) => {
+
+    const location = useLocation();
+
+    const isActive = props.to === location.pathname;
+
+    return (
+        <div className={"item" + (isActive ? " is-active" : "")}>
+            <Link {...props}>
+                <FontAwesomeIcon icon={icon} />
+            </Link>
+        </div>
+    )
+
+}
+
 function Toolbar() {
 
     useToolbarOffset(55);
 
+    const links = [
+        { to: "/", "aria-label": "Dashboard home", icon: "home" },
+        { to: "/team", "aria-label": "Manage team", icon: "users" },
+        { to: "/students", "aria-label": "Manage students", icon: "user-graduate" }
+    ];
+
     return (
         <div className="toolbar has-background-white">
-            <div className="item">
-                <Link to="/" aria-label="Dashboard home">
-                    <FontAwesomeIcon icon="home" />
-                </Link>
-            </div>
-            <div className="item">
-                <Link to="/team" aria-label="Manage team">
-                    <FontAwesomeIcon icon="users" />
-                </Link>
-            </div>
-            <div className="item">
-                <Link to="/students" aria-label="Manage students">
-                    <FontAwesomeIcon icon="user-graduate" />
-                </Link>
-            </div>
+            { links.map( link => <ToolbarItem key={link.to} {...link} /> ) }
             <div className="item end">
                 <LogoutLink aria-label="Logout">
                     <FontAwesomeIcon icon={['far','arrow-alt-circle-left']} />
