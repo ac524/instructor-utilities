dc858e562997de2d357c0d0a0f1076088f366ca3
Anthony Brown <ac524.brown@gmail.com>
2020-09-02 10:30:51 -0700

    Refactor passwordHash funciton to simplify structure into a one line arrow return.

--- app/config/utils/passwordHash.js
+++ app/config/utils/passwordHash.js
@@ -4,10 +4,4 @@ const util = require('util');
 const genSalt = util.promisify( bcrypt.genSalt );
 const hash = util.promisify( bcrypt.hash );
 
-module.exports = async ( raw ) => {
-
-    const salt = await genSalt( 10 );
-
-    return hash( raw, salt );
-
-}
+module.exports = async ( raw ) => hash( raw, await genSalt( 10 ) );
