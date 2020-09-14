cd64b93cf7c89c73429033a2ddefbbb662fa00c8
Anthony Brown <ac524.brown@gmail.com>
2020-09-13 22:54:51 -0700

    Refactored registration validation to make checks more versatile.

--- app/config/validation/register.js
+++ app/config/validation/register.js
@@ -1,47 +1,64 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-module.exports = function validateRegisterInput( data ) {
+const checks = {
+  name: ( data, errors ) => {
+
+    data.name = !isEmpty(data.name) ? data.name : "";
+
+    if (Validator.isEmpty(data.name)) {
+      errors.name = "Name field is required";
+    }
+
+  },
+  email: ( data, errors ) => {
+
+    data.email = !isEmpty(data.email) ? data.email : "";
+
+    if (Validator.isEmpty(data.email)) {
+      errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+      errors.email = "Email is invalid";
+    }
+
+  },
+  password: ( data, errors ) => {
+
+    data.password = !isEmpty(data.password) ? data.password : "";
+    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+    if (Validator.isEmpty(data.password)) {
+      errors.password = "Password field is required";
+    }
+    if (Validator.isEmpty(data.password2)) {
+      errors.password2 = "Confirm password field is required";
+    }
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+      errors.password = "Password must be at least 6 characters";
+    }
+    if (!Validator.equals(data.password, data.password2)) {
+      errors.password2 = "Passwords must match";
+    }
+
+  },
+  roomname: ( data, errors ) => {
+
+    data.roomname = !isEmpty(data.roomname) ? data.roomname : "";
+
+    if (Validator.isEmpty(data.roomname)) {
+      errors.roomname = "Classroom name is required";
+    }
+
+  }
+}
+
+module.exports = function validateRegisterInput( data, skips = [] ) {
 
   let errors = {};
 
-  // Convert empty fields to an empty string so we can use validator functions
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-  data.roomname = !isEmpty(data.roomname) ? data.roomname : "";
-
-  // Name checks
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
-  }
-
-  // Email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
-  }
-
-  // Password checks
-  if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
-  }
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm password field is required";
-  }
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
-  }
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
-  }
-
-  // Room name checks
-  if (Validator.isEmpty(data.roomname)) {
-    errors.roomname = "Classroom name is required";
-  }
+  Object.keys( checks )
+    .filter( check => !skips.includes(check) )
+    .forEach( check => checks[check]( data, errors ) )
 
   return {
     errors,
