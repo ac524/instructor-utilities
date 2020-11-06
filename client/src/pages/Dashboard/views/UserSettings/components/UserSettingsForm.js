0b762ab5ce2209d82673f22dd9e962b8585731ce
bbtwots <brettbelka@gmail.com>
2020-11-05 20:57:55 -0800

    simple change password in user settings

--- client/src/pages/Dashboard/views/UserSettings/components/UserSettingsForm.js
+++ client/src/pages/Dashboard/views/UserSettings/components/UserSettingsForm.js
@@ -9,25 +9,41 @@ import { UPDATE_USER } from "store/actions";
 const validateUserSettingsData = createValidator({
     validators: {
         name: ({ name }) => Boolean(name) || "Your name is required",
-        email: ({ email }) => Boolean(email) || "Email is required"
+        email: ({ email }) => Boolean(email) || "Email is required",
+        password: ({ password, password2 }) => {
+
+            const errors = [];
+
+            if (password || password2) {
+
+                // TODO stronger password validation for both client and server.
+                if (password.length < 6) errors.push(["password", "Password must be at least 6 characters"]);
+
+                if (password2 && (password2 != password)) errors.push(["password2", "Confirm password must match"]);
+            };
+
+            if (errors.length) return Object.fromEntries(errors);
+        },
     }
 });
 
 export const useUserSettingFields = () => {
 
-    const [ fields, setFields ] = useState([]);
-    const [ state, setState ] = useState({});
+    const [fields, setFields] = useState([]);
+    const [state, setState] = useState({});
 
     const user = useAuthorizedUser();
 
     useEffect(() => {
 
-        if(!user) return;
+        if (!user) return;
 
         setState({
             name: user.name,
-            email: user.email
-        }); 
+            email: user.email,
+            password: "",
+            password2: ""
+        });
 
     }, [user, setState]);
 
@@ -47,12 +63,26 @@ export const useUserSettingFields = () => {
                 onChange,
                 value: state.email,
                 name: "email"
+            },
+            {
+                label: "Password",
+                onChange,
+                value: state.password,
+                type: "password",
+                name: "password"
+            },
+            {
+                label: "Confirm Password",
+                onChange,
+                value: state.password2,
+                type: "password",
+                name: "password2"
             }
         ])
 
     }, [state, setState, setFields]);
 
-    return [ fields, state ];
+    return [fields, state];
 
 }
 
@@ -60,22 +90,22 @@ const UserSettingsForm = () => {
 
     const dispatch = useStoreDispatch();
     const user = useAuthorizedUser();
-    const [ fields, values ] = useUserSettingFields();
-    const [ errors, setErrors ] = useState();
+    const [fields, values] = useUserSettingFields();
+    const [errors, setErrors] = useState();
 
     const handleSubmit = async e => {
 
         e.preventDefault();
 
-        const updateList = Object.entries(values).filter( ([key,value]) => value !== user[key] );
+        const updateList = Object.entries(values).filter(([key, value]) => value !== user[key]);
 
-        if( updateList.length ) {
+        if (updateList.length) {
 
-            const updates = Object.fromEntries( updateList );
+            const updates = Object.fromEntries(updateList);
 
-            const [ data, errors, hasErrors ] = validateUserSettingsData( updates );
+            const [data, errors, hasErrors] = validateUserSettingsData(updates);
 
-            if( hasErrors ) {
+            if (hasErrors) {
                 setErrors(errors);
                 return;
             }
@@ -84,16 +114,16 @@ const UserSettingsForm = () => {
 
             try {
 
-                await api.updateUser( data );
+                await api.updateUser(data);
 
-            } catch(err) {
+                //Clearing the errors this feels a little hacky but it's seems to behave reasonably well.
+                setErrors();
 
-                if( err.response ) setErrors( err.response.data );
+            } catch (err) {
 
+                if (err.response) setErrors(err.response.data);
             }
-
         }
-
     }
 
     return <Form flat fields={fields} errors={errors} onSubmit={handleSubmit} buttonText="Save" />;
