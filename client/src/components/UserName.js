f875526def204095cd0abc29d9c96fc4a3a0effb
Anthony Brown <ac524.brown@gmail.com>
2020-11-13 19:00:00 -0800

    Cleaned up left over console logs from dev.

--- client/src/components/UserName.js
+++ client/src/components/UserName.js
@@ -3,8 +3,6 @@ import { useAuthorizedUser } from "utils/auth";
 
 const UserName = ({ user: {_id, name }, ...props }) => {
 
-    console.log(_id, name);
-
     const { _id: currentUserId } = useAuthorizedUser();
 
     return <span {...props}>{ _id === currentUserId ? "You" : name }</span>;
