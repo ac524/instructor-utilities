8306d87bc134d0b89442c448eff0a9eef8917f14
Anthony Brown <ac524.brown@gmail.com>
2020-11-18 12:13:40 -0800

    Updated email validation route/ctrl to new approach.

--- app/routes/validate.js
+++ app/routes/validate.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const isAuthenticated = require("./middleware/isAuthenticated");
 
+const cch = require("./middleware/createControllerHandler");
+const sde = require("./middleware/setDefaultError");
+
 const {
     validate,
     resend
 } = require("../controllers/emailValidation");
 
-router.post( "/resend", isAuthenticated, resend );
-router.post( "/:token", validate );
+router.post( "/resend", isAuthenticated, sde("An error occured resending the email."), cch( resend ) );
+router.post( "/:token", sde("An error occured validating the email."), cch( validate ) );
 
 module.exports = router;
