eb22b678207ca49fbcef730ec2014f9838a88e42
Anthony Brown <ac524.brown@gmail.com>
2020-11-19 00:31:18 -0800

    Update room ctrl/route to new approach.

--- app/routes/isAuth/rooms.js
+++ app/routes/isAuth/rooms.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const setRoom = require("../middleware/setRoom");
 const isRoomMember = require("../middleware/isRoomMember");
 
+const cch = require("../middleware/createControllerHandler");
+const sde = require("../middleware/setDefaultError");
+
 const {
     getSingle,
     update
@@ -10,7 +13,7 @@ const {
 
 router
     .route( "/:roomId" )
-    .get( setRoom.fromParam, isRoomMember, getSingle )
-    .patch( setRoom.fromParam, isRoomMember, update );
+    .get( setRoom.fromParam, isRoomMember, cch( getSingle ) )
+    .patch( setRoom.fromParam, isRoomMember, cch( update, { include: ["roomStaffMember"] } ) );
 
 module.exports = router;
