bb8d2ad635f8db4e5f9fe22956fcbe52a5f49930
Christopher Marti <chrisjm093@gmail.com>
2020-11-24 18:16:12 -0800

    added nyc testing report package

--- test/unit/authCtrl.js
+++ test/unit/authCtrl.js
@@ -2,15 +2,19 @@ const chai = require("chai");
 const { authenticated, login } = require("../../app/controllers/auth");
 const { User } = require("../../app/models");
 const assert = require('assert');
-const expect = require('expect');
+const expect = chai.expect
 
 
 describe('Auth Controller', function() {
   describe('#authenticated()', function() {
-    it('should return user object', function() {
-     const user = new User ({name: "bob"})
+    it('should return user object', function(done) {
+      const user = new User ({name: "bob"});
       
-      .
+      const result = authenticated({ user });
+
+      expect(result)
+      .to.be.an("object");
+      return done()
     });
   });
 });
