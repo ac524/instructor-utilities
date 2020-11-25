32be709b53343f3563bf55e563438cbf8718ca7f
Anthony Brown <ac524.brown@gmail.com>
2020-11-24 16:59:20 -0800

    comment updates.

--- app/config/permissions/PermissionSet.js
+++ app/config/permissions/PermissionSet.js
@@ -8,9 +8,10 @@ class PermissionSet {
 
     constructor( enabled ) {
 
-
+        // Toggle enabled keys to true.
         for( const key of enabled ) this[key] = true;
 
+        // Validate all object properties, setting them explicity to `false` if the key is not `true`, so that all keys will be a Boolen type value.
         for( const key of Object.keys(this) ) if( !this[key] ) this[key] = false;
 
     }
