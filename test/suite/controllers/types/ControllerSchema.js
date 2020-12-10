7e8f4503bc757b2b704df8c7f6248f916310baf9
Anthony Brown <ac524.brown@gmail.com>
2020-12-10 00:00:29 -0800

    Created some first working tests around the  function that requires function mocks and spies for testing.

--- test/suite/controllers/types/ControllerSchema.js
+++ test/suite/controllers/types/ControllerSchema.js
@@ -1,19 +1,18 @@
 const { expect } = require("chai");
-// const sinon = require("sinon");
+const sinon = require("sinon");
 
 const mongoose = require( "mongoose" );
-const { Model, Document } = mongoose;
+const { Document } = mongoose;
 
 const TestModel = require("../../../lib/TestModel");
 
 const SchemaController = require("../../../../app/controllers/types/SchemaController");
 
-
-// after(function () {
-//   sinon.restore();
-// });
-
 describe("SchemaController", function() {
+
+    const sandbox = sinon.createSandbox();
+
+    afterEach(() => sandbox.restore());
 
     describe("constructor()", function() {
   
@@ -87,6 +86,81 @@ describe("SchemaController", function() {
 
         // Assert
         expect( doc ).to.include( test );
+
+        done();
+
+      });
+
+    });
+
+    describe("createOne()", function() {
+
+      /**
+       * Stubs the makeDoc function after using it to create a document that can be safely stubbed and spied on.
+       * @param {SchemaController} ctrl
+       * 
+       * @returns {Document}
+       */
+      const makeDocHelper = ( ctrl, data ) => {
+
+        const doc = ctrl.makeDoc( data );
+
+        sandbox.stub(ctrl, "makeDoc").callsFake(() => doc);
+        sandbox.stub(doc, "save").callsFake(() => {});
+
+        return doc;
+
+      }
+
+      it("should call the `makeDoc` method with the given data", (done) => {
+
+        // Arrange
+        const ctrl = new SchemaController( "modelkey", TestModel );
+        const test = { name: "A test" };
+
+        makeDocHelper( ctrl, test );
+
+        // Act
+        ctrl.createOne( { data: test } );
+
+        // Assert
+        expect( ctrl.makeDoc.calledWithExactly( test ) ).to.be.true;
+
+        done();
+
+      });
+
+      it("should call the document's save method by default", (done) => {
+
+        // Arrange
+        const ctrl = new SchemaController( "modelkey", TestModel );
+        const test = { name: "A test" };
+
+        const doc = makeDocHelper( ctrl, test );
+
+        // Act
+        ctrl.createOne( { data: test } );
+
+        // Assert
+        expect( doc.save.calledOnce ).to.be.true;
+
+        done();
+
+      });
+
+      it("should not call the document's save if configured with `save` set to false", (done) => {
+
+        // Arrange
+        const ctrl = new SchemaController( "modelkey", TestModel );
+        const test = { name: "A test" };
+
+        const doc = makeDocHelper( ctrl, test );
+
+        // Act
+        ctrl.createOne( { data: test }, { save: false } );
+
+        // Assert
+        expect( doc.save.calledOnce ).to.be.false;
 
         done();
 
