dee38a6301c5aafa6a33b7d4be9456d3aaddf781
Anthony Brown <ac524.brown@gmail.com>
2020-12-11 10:07:54 -0800

    Added some comments and moved the construction of the mocha instance to the scan dir callback.

--- test/run.js
+++ test/run.js
@@ -2,13 +2,21 @@ const fs    = require ('fs');
 const path  = require('path');
 const util = require('util');
 const mocha = require('mocha');
-const suite = new mocha();
 
 const readDir = util.promisify( fs.readdir );
 
+// Starting diretor to scan.
 const dirname = "suite";
 
-const ignoreMap = new Map([ ["suite",1] ]);
+// A map of folder names to ignore during the scan for test files.
+const ignoreMap = new Map(
+  [
+
+    // Folder name ignore list.
+    "suite"
+
+  ].map(filename=>[filename,1])
+);
 
 // Recursively looks for files in folders and builds on list of all found .js files.
 const getDirFiles = async folder => {
@@ -37,6 +45,8 @@ const getDirFiles = async folder => {
 
 getDirFiles( path.join(__dirname, dirname) )
   .then(files => {
+
+    const suite = new mocha();
 
     // Add each found file to the suite.
     files.forEach(file => suite.addFile( file ));
