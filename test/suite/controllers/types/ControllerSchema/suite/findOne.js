b337d4af116144285a0b2e181237bc8a9bcef861
Anthony Brown <ac524.brown@gmail.com>
2020-12-13 00:09:55 -0800

    Added findOne tests around what happens if both id and search are passed in. Added another layer of describe in findOne to break up the major segments and improve test organization.

--- test/suite/controllers/types/ControllerSchema/suite/findOne.js
+++ test/suite/controllers/types/ControllerSchema/suite/findOne.js
@@ -39,113 +39,149 @@ module.exports = function() {
 
     after(() => sandbox.restore());
 
-    /**
-     * START: Testing usage with `docId`
-     */
+    describe("By `docId`", () => {
 
-    it( "should call the `query` method with a `DocumentQuery` and provided `queryOptions` when passed a `docId`", () => {
+        it( "should call the `query` method with a `DocumentQuery` and provided `queryOptions` when passed a `docId`", () => {
 
-        // Arrange
-        const ctrl = makeCtrl();
-        const docId = new ObjectId();
-        const queryOptions = { select: "name" };
-
-        // Act
-        ctrl.findOne( { docId }, queryOptions );
-
-        // Assert
-        /**
-         * TODO  Unsure on how to test the first param at the moment. You can't seem to just create a
-         * `new DocumentQuery` for the stub method, so more exploration will be needed to solve.
-         */
-        // expect( ctrl.query.getCall(0).args[0] ).to.be.instanceof( DocumentQuery );
-        expect( ctrl.query.args[0][1] ).to.equal( queryOptions );
-
-    } );
-
-    it( "should call the model's `findById` method with `docId` when provided", () => {
-
-        // Arrange
-        const ctrl = makeCtrl();
-        const docId = new ObjectId();
-
-        // Act
-        ctrl.findOne( { docId } );
-
-        // Assert
-        expect( TestModel.findById.calledWithExactly(docId) ).to.be.true;
-
-    });
-
-    it( "should return a `Document` when passed an existing `docId`", async () => {
-
-        // Arrange
-        const ctrl = makeCtrl();
-        const doc = new TestModel({name: "A test"});
-        const docId = doc._id;
-
-        ctrl.query.callsFake(() => doc);
-
-        // Act
-        const foundDoc = await ctrl.findOne( { docId } );
-
-        // Assert
-        expect( foundDoc ).to.equal( doc );
-
-    });
-
-    /**
-     * START: Testing usage with `search`
-     */
-
-    it( "should call the `query` method with a `DocumentQuery` and provided `queryOptions` when passed a `search`", () => {
-
-        // Arrange
-        const ctrl = makeCtrl();
-        const search = { name: "A test" };
-        const queryOptions = { select: "name" };
-
-        // Act
-        ctrl.findOne( { search }, queryOptions );
-
-        // Assert
-        /**
-         * TODO Unsure on how to test the first param at the moment. You can't seem to just create a
-         * `new DocumentQuery` for the stub method, so more exploration will be needed to solve.
-         */
-        // expect( ctrl.query.getCall(0).args[0] ).to.be.instanceof( DocumentQuery );
-        expect( ctrl.query.args[0][1] ).to.equal( queryOptions );
-
-    } );
-
-    it( "should call the model's `findOne` method with `search` when provided", () => {
-
-        // Arrange
-        const ctrl = makeCtrl();
-        const search = { name: "A test" };
-
-        // Act
-        ctrl.findOne( { search } );
-
-        // Assert
-        expect( TestModel.findOne.calledWithExactly(search) ).to.be.true;
+            // Arrange
+            const ctrl = makeCtrl();
+            const docId = new ObjectId();
+            const queryOptions = { select: "name" };
+    
+            // Act
+            ctrl.findOne( { docId }, queryOptions );
+    
+            // Assert
+            /**
+             * TODO  Unsure on how to test the first param at the moment. You can't seem to just create a
+             * `new DocumentQuery` for the stub method, so more exploration will be needed to solve.
+             */
+            // expect( ctrl.query.getCall(0).args[0] ).to.be.instanceof( DocumentQuery );
+            expect( ctrl.query.args[0][1] ).to.equal( queryOptions );
+    
+        } );
+    
+        it( "should call the model's `findById` method with `docId` when provided", () => {
+    
+            // Arrange
+            const ctrl = makeCtrl();
+            const docId = new ObjectId();
+    
+            // Act
+            ctrl.findOne( { docId } );
+    
+            // Assert
+            expect( TestModel.findById.calledWithExactly(docId) ).to.be.true;
+    
+        });
+    
+        it( "should return a `Document` when passed an existing `docId`", async () => {
+    
+            // Arrange
+            const ctrl = makeCtrl();
+            const doc = new TestModel({name: "A test"});
+            const docId = doc._id;
+    
+            ctrl.query.callsFake(() => doc);
+    
+            // Act
+            const foundDoc = await ctrl.findOne( { docId } );
+    
+            // Assert
+            expect( foundDoc ).to.equal( doc );
+    
+        });
+    
 
     });
 
-    it( "should return a `Document` when passed a `search` for an existing document", async () => {
+    describe("By `search`", () => {
 
-        // Arrange
-        const ctrl = makeCtrl();
-        const doc = new TestModel({name: "A test"});
-        const search = { name: doc.name };
+        it( "should call the `query` method with a `DocumentQuery` and provided `queryOptions` when passed a `search`", () => {
 
-        ctrl.query.callsFake(() => doc);
+            // Arrange
+            const ctrl = makeCtrl();
+            const search = { name: "A test" };
+            const queryOptions = { select: "name" };
 
-        // Act
-        const foundDoc = await ctrl.findOne( { search } );
+            // Act
+            ctrl.findOne( { search }, queryOptions );
 
-        // Assert
-        expect( foundDoc ).to.equal( doc );
+            // Assert
+            /**
+             * TODO Unsure on how to test the first param at the moment. You can't seem to just create a
+             * `new DocumentQuery` for the stub method, so more exploration will be needed to solve.
+             */
+            // expect( ctrl.query.getCall(0).args[0] ).to.be.instanceof( DocumentQuery );
+            expect( ctrl.query.args[0][1] ).to.equal( queryOptions );
+
+        } );
+
+        it( "should call the model's `findOne` method with `search` when provided", () => {
+
+            // Arrange
+            const ctrl = makeCtrl();
+            const search = { name: "A test" };
+
+            // Act
+            ctrl.findOne( { search } );
+
+            // Assert
+            expect( TestModel.findOne.calledWithExactly(search) ).to.be.true;
+
+        });
+
+        it( "should return a `Document` when passed a `search` for an existing document", async () => {
+
+            // Arrange
+            const ctrl = makeCtrl();
+            const doc = new TestModel({name: "A test"});
+            const search = { name: doc.name };
+
+            ctrl.query.callsFake(() => doc);
+
+            // Act
+            const foundDoc = await ctrl.findOne( { search } );
+
+            // Assert
+            expect( foundDoc ).to.equal( doc );
+
+        });
+
+    });
+
+    // Rename to something like priority testing?
+    describe("By `docId` and `search`", () => {
+
+        it( "should call the model's findById method if provided a `docId` and `search`", async () => {
+
+            // Arrange
+            const ctrl = makeCtrl();
+            const docId = new ObjectId();
+            const search = { name: "A test" };
+
+            // Act
+            ctrl.findOne( { docId, search } );
+
+            // Assert
+            expect( TestModel.findById.calledOnce ).to.be.true;
+
+        });
+
+        it( "should not call the model's findOne method if provided a `docId` and `search`", async () => {
+
+            // Arrange
+            const ctrl = makeCtrl();
+            const docId = new ObjectId();
+            const search = { name: "A test" };
+
+            // Act
+            ctrl.findOne( { docId, search } );
+
+            // Assert
+            expect( TestModel.findOne.calledOnce ).to.be.false;
+
+        });
 
     });
 
