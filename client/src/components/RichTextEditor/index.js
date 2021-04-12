f9ec11e7ad75ccc3ccb55c825c5ccdd3da68bac7
Joshua J. Haller <phone@joshuajhaller.com>
2021-04-12 16:53:17 -0700

    Text editor converted from class component to functional component.

--- client/src/components/RichTextEditor/index.js
+++ client/src/components/RichTextEditor/index.js
@@ -1,11 +1,47 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import DOMPurify from "dompurify";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./RichText.sass";
-import "../../node_modules/draft-js/dist/Draft.css";
-
 import ReactDOM from "react-dom";
-import { Editor, EditorState } from "draft-js";
 
-const RichTextEditor = () => {};
+const RichTextEditor = () => {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  };
+
+  const createMarkup = (html) => {
+    return {
+      __html: DOMPurify.sanitize(html),
+    };
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">Rich Text Editor Example</header>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={handleEditorChange}
+      />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}
+      ></div>
+    </div>
+  );
+};
 
 export default RichTextEditor;
