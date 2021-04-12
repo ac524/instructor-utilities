4430c8a9fd1512b03b7ef3b1f20939e7e1003519
Anthony Brown <ac524.brown@gmail.com>
2021-04-11 18:28:36 -0700

    Some small whitespace adjustment, removed used import.

--- client/src/pages/Dashboard/views/UserSettings/components/NoClassroomNotification.js
+++ client/src/pages/Dashboard/views/UserSettings/components/NoClassroomNotification.js
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
-
 import {
     Message
 } from "react-bulma-components";
+
 import { useAuthorizedUser } from "utils/auth";
 
-
 const NoClassroomNotification = () => {
-
 
     const user = useAuthorizedUser();
 
@@ -17,6 +14,8 @@ const NoClassroomNotification = () => {
                 Hey there, {user.name}! Looks like you don't belong to any classrooms at the moment. This means you'll only have access to your settings until you either create a new classroom or accept an invite as a TA to a new room.
             </Message.Body>
         </Message>
-    )
+    );
+
 }
+
 export default NoClassroomNotification;
