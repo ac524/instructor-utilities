25c869be5e4d0ea2bff3b05b45af3a46a3b4b961
daniel <danielsoledad@gmail.com>
2021-04-14 01:15:19 -0700

    conveting invite.js, regiter.js, and validateEmail.js to Controllers

---
+++ app/controllers/definitions/InviteController.js
@@ -0,0 +1,252 @@
+/** @format */
+
+const mail = require("../../mail");
+
+const homeUrl = require("../../config/options")("publicUrl");
+const {
+	InvalidDataError,
+	InvalidUserError,
+	NotFoundError
+} = require("../../config/errors");
+
+const ioEmit = require("../utils/ioEmit");
+const Controller = require("../types/Controller");
+
+/**
+ * Type Definition Imports
+ * @typedef {import('mongoose').Schema.Types.ObjectId} ObjectId
+ *
+ * @typedef {import('../controllers/models/schema/MemberSchema').MemberDocument} MemberDocument
+ * @typedef {import('../controllers/models/schema/RoomSchema').RoomDocument} RoomDocument
+ * @typedef {import('../controllers/models/schema/UserSchema').UserDocument} UserDocument
+ * @typedef {import('../controllers/models/schema/InviteSchema').InviteDocument} InviteDocument
+ * @typedef {import('../controllers/models/schema/TokenSchema').TokenDocument} TokenDocument
+ *
+ * @typedef {import('~crsm/routes/validation/definitions/inviteValidation').InviteData} InviteData
+ * @typedef {import('~crsm/routes/validation/definitions/registerValidation').RegistrationData} RegistrationData
+ */
+
+/**
+ * Type Definitions
+ */
+
+class InviteController extends Controller {
+	constructor() {
+
+		super("invite");
+        
+	}
+
+	/**
+	 * @param {ObjectId} roomId
+	 * @param {MemberDocument} member
+	 */
+	async addStaff(roomId, member) {
+		const { staff } = await this.effect("room").updateOne(
+			{
+				docId: roomId,
+				data: { $push: { staff: member } }
+			},
+			{
+				select: "staff",
+				populate: "staff.user"
+			}
+		);
+
+		return staff[staff.length - 1];
+	}
+
+	/**
+	 * @param {RoomDocument} room
+	 * @param {InviteDocument} invite
+	 * @param {UserDocument} from
+	 */
+	sendInvite(room, invite, from) {
+		return mail.send(
+			"invite",
+			{
+				name: from.name,
+				roomName: room.name,
+				inviteLink: `${homeUrl}/invite/${invite.token.tokenString}`
+			},
+			{
+				to: invite.email,
+				subject: `You've been invited to Classroom by ${from.name}!`
+			}
+		);
+	}
+
+	/** CONTROLLER METHODS **/
+
+	/**
+	 * @typedef CreateRoomInviteOptions
+	 * @property {ObjectId} roomId
+	 * @property {UserDocument} user
+	 * @property {InviteData} inviteData
+	 *
+	 * @param {CreateRoomInviteOptions} param0
+	 */
+	async create({ roomId, user, inviteData }) {
+		const roomEmails = await this.effect("room").findOne(
+			{ docId: roomId },
+			{
+				populate: ["staff.user", "email"],
+				select: "invites.email"
+			}
+		);
+
+		const { email } = inviteData;
+
+		// Check if the email is already registered to a staff member.
+		if (roomEmails.staff.map(({ user }) => user.email).includes(email))
+			throw new InvalidDataError("Unable to create invite.", {
+				email: "This email is already registered to a staff member."
+			});
+
+		// Check if the email is already registered to an invite.
+		if (
+			roomEmails.invites &&
+			roomEmails.invites.map(({ email }) => email).includes(email)
+		)
+			throw new InvalidDataError("Unable to create invite.", {
+				email: "This email already has an invite."
+			});
+
+		const token = await this.effect("token").createOne({ data: { relation: roomId } });
+
+		const update = {
+			$push: {
+				invites: {
+					email,
+					token: token._id
+				}
+			}
+		};
+
+		const room = await this.effect("room").updateOne(
+			{
+				docId: roomId,
+				data: update
+			},
+			{
+				populate: "invites.token"
+			}
+		);
+
+		const invite = room.invites[room.invites.length - 1];
+
+		if (mail.isEnabled) await sendInvite(room, invite, user);
+
+		return invite;
+	}
+
+    /**
+     * @typedef RemoveRoomInviteOptions
+     * @property {ObjectId} roomId
+     * @property {ObjectId} inviteId
+     *
+     * @param {RemoveRoomInviteOptions} param0
+     */
+    async remove ({ roomId, inviteId }) {
+        const room = await this.effect("room").findOne({ docId: roomId });
+
+        const invite = room.invites.id(inviteId);
+
+        if (!invite) throw new NotFoundError("Invite not found.");
+
+        await this.effect("token").deleteOne({ docId: invite.token });
+
+        invite.remove();
+
+        await room.save();
+    }
+
+    /**
+     * @typedef GetInviteEmailCheckOptions
+     * @property {InviteDocument} invite
+     *
+     * @param {GetInviteEmailCheckOptions} param0
+     */
+    async emailCheck ( { invite } ) {
+        const { email } = invite;
+
+        const user = await this.effect("user").findOne({ search: { email } });
+
+        return {
+            hasUser: Boolean(user),
+            email
+        };
+    }
+
+    /**
+     * @typedef RegisterInviteOptions
+     * @property {InviteDocument} invite
+     * @property {RegistrationData} registerData
+     *
+     * @param {RegisterInviteOptions} param0
+     */
+    async register ({ invite, registerData }) {
+        const { email } = invite;
+
+        const existingUser = await this.effect("user").findOne({ search: { email } });
+
+        if (existingUser)
+            throw new InvalidDataError("Invalid registration.", {
+                email: "Email already exists"
+            });
+
+        const { name, password } = registerData;
+
+        // Create the User
+        await this.effect("user").createOne({
+            data: {
+                name,
+                email,
+                password,
+                isVerified: true
+            }
+        });
+    }
+    
+    /**
+     * @typedef AcceptInviteOptions
+     * @property {TokenDocument} inviteToken
+     * @property {RoomDocument} inviteRoom
+     * @property {InviteDocument} invite
+     * @property {UserDocument} user
+     *
+     * @param {AcceptInviteOptions} param0
+     */
+    async accept ( { inviteToken, inviteRoom, invite, user } ) {
+        const roomId = inviteRoom._id;
+
+        if (user.email !== invite.email)
+            throw new InvalidUserError("Please log into the correct account.");
+
+        // Add the staff member to the classroom
+        const staff = await addStaff(roomId, {
+            role: "ta",
+            user: user._id
+        });
+
+        // Add the room to the user
+        await user.update({ $push: { classrooms: roomId } });
+
+        const addStaffDispatch = { type: "ADD_STAFF", payload: staff };
+        ioEmit("dispatch", addStaffDispatch, `room:${roomId}`);
+
+        await inviteToken.remove();
+
+        invite.remove();
+
+        await inviteRoom.save();
+
+        const deleteInviteDispatch = { type: "DELETE_INVITE", payload: invite._id };
+        ioEmit("dispatch", deleteInviteDispatch, `room:${roomId}`);
+
+        return { success: true, roomId: roomId };
+    }
+}
+
+
+module.exports = InviteController;
---
+++ app/controllers/definitions/ValidateEmailController.js
@@ -0,0 +1,63 @@
+/** @format */
+
+const sendUserVerifyEmail = require("../utils/sendUserVerifyEmail");
+const ioEmit = require("../utils/ioEmit");
+
+const tokenCtrl = require("../token");
+const userCtrl = require("../user");
+
+const { NotFoundError } = require("../../config/errors");
+const Controller = require("../types/Controller");
+/**
+ * TYPE DEFINITION IMPORTS
+ * @typedef {import('~/routes/validation/definitions/resendValidation').ResendData} ResendData
+ */
+
+/**
+ * Type Definitions
+ */
+class ValidateEmailController extends Controller {
+	constructor() {
+		super("validate.email");
+	}
+
+    /**
+     * @typedef {Object} ResendValidateEmailOptions
+     * @property {ResendData} config
+     *
+     * @param {ResendValidateEmailOptions} param0
+     */
+    async resend ({ config }) {
+        const { email } = config;
+
+        const user = await userCtrl.findOne({ search: { email } });
+
+        if (!user) throw new NotFoundError("Email not found.");
+
+        await sendUserVerifyEmail(user);
+    };
+
+    /**
+     * @typedef {Object} ValidateEmailOptions
+     * @property {string} token
+     *
+     * @param {ValidateEmailOptions} param0
+     */
+    async validate ({ tokenString }) {
+        const tokenRecord = await tokenCtrl.getByTokenString({ tokenString });
+
+        if (!tokenRecord) throw new NotFoundError("Token not found.");
+
+        const update = { isVerified: true };
+
+        await userCtrl.updateOne({
+            docId: tokenRecord.relation,
+            data: update
+        });
+
+        ioEmit("user:update", { isVerified: true }, `user:${tokenRecord.relation}`);
+    };
+}
+
+
+module.exports = ValidateEmailController;
