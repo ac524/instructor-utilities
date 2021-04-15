94c18cd2cefa94f8417f5c0b119a7bee98ffce2f
Anthony Brown <ac524.brown@gmail.com>
2021-04-15 10:31:18 -0700

    Removed imports for token and user controllers and updated usages to access them through the 'effect' function

--- app/controllers/definitions/ValidateEmailController.js
+++ app/controllers/definitions/ValidateEmailController.js
@@ -1,9 +1,6 @@
 const sendUserVerifyEmail = require("../utils/sendUserVerifyEmail");
 const ioEmit = require("../utils/ioEmit");
 
-const tokenCtrl = require("../token");
-const userCtrl = require("../user");
-
 const { NotFoundError } = require("../../config/errors");
 const Controller = require("../types/Controller");
 /**
@@ -35,7 +32,7 @@ class ValidateEmailController extends Controller {
     async resend ({ config }) {
         const { email } = config;
 
-        const user = await userCtrl.findOne({ search: { email } });
+        const user = await this.effect('user').findOne({ search: { email } });
 
         if (!user) throw new NotFoundError("Email not found.");
 
@@ -46,13 +43,13 @@ class ValidateEmailController extends Controller {
      * @param {ValidateEmailOptions} param0
      */
     async validate ({ tokenString }) {
-        const tokenRecord = await tokenCtrl.getByTokenString({ tokenString });
+        const tokenRecord = await this.effect('token').getByTokenString({ tokenString });
 
         if (!tokenRecord) throw new NotFoundError("Token not found.");
 
         const update = { isVerified: true };
 
-        await userCtrl.updateOne({
+        await this.effect('user').updateOne({
             docId: tokenRecord.relation,
             data: update
         });
