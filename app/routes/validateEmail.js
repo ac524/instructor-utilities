7cb72c3d3cb65c2941d2896a9d222ec8e596d0ab
Anthony Brown <ac524.brown@gmail.com>
2021-04-18 00:12:19 -0700

    Fix resend route - Extended `addRequest` config with new `unverified` property. When set to true, the `isVerified` middleware will be left off of the authentication steps. - Added `unverified` to the `validateemail/resend` route. - Fixed resend route validation to check for the correct `email` field instead of `type` - Added body:data key map as a default map for all routes.

--- app/routes/validateEmail.js
+++ app/routes/validateEmail.js
@@ -9,6 +9,7 @@ module.exports = createRouter([
     ["/resend", {
         post: {
             auth: true,
+            unverified: true,
             defaultError: "resend the email",
             validation: resendVal,
             ctrl: validateEmailCtrl.binding.resend
