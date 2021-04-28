c6693f06765f053dfbc342c30b5b4f7fbaa59132
daniel <danielsoledad@gmail.com>
2021-04-27 17:53:02 -0700

    modified:   app/routes/_auth.js
        * required library instead of  auth controller
    
        * updated code that used authCtrl to library.get("auth")

--- app/routes/_auth.js
+++ app/routes/_auth.js
@@ -2,28 +2,32 @@ const createRouter = require("./utils/createRouter");
 
 const { login: loginVal } = require("./validation")
 
-const { authCtrl } = require("../controllers");
+const library = require("../controllers");
 
 const loginCtlrConfig = {
     keyMap: { body: "credentials" }
 };
 
 module.exports = createRouter([
+	[
+		"/login",
+		{
+			post: {
+				defaultError: "login",
+				validation: loginVal,
+				ctrl: [library.get("auth").binding.login, loginCtlrConfig]
+			}
+		}
+	],
 
-    ["/login", {
-        post: {
-            defaultError: "login",
-            validation: loginVal,
-            ctrl: [ authCtrl.binding.login, loginCtlrConfig ]
-        }
-    }],
-
-    ["/authenticated", {
-        post: {
-            defaultError: "get the authenticated user",
-            auth: true,
-            ctrl: authCtrl.binding.authenticated
-        }
-    }]
-
+	[
+		"/authenticated",
+		{
+			post: {
+				defaultError: "get the authenticated user",
+				auth: true,
+				ctrl: library.get("auth").binding.authenticated
+			}
+		}
+	]
 ]);
