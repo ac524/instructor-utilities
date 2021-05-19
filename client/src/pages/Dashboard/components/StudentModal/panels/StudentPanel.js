78cacdf70dc1f9e21a05979e1f67052da14ff19c
daniel <danielsoledad@gmail.com>
2021-05-19 01:21:22 -0700

    modified:   client/src/pages/Dashboard/components/StudentModal/index.sass     Updated sass file to remove the padding of column when tabs are active modified:   client/src/pages/Dashboard/components/StudentModal/panels/StudentPanel.js     flip the logic to have rounded corners on the modal

--- client/src/pages/Dashboard/components/StudentModal/panels/StudentPanel.js
+++ client/src/pages/Dashboard/components/StudentModal/panels/StudentPanel.js
@@ -29,45 +29,39 @@ export const StudentPanel = ( { roomId, student } ) => {
     const toggleBulkCreate = () => setIsBulkCreate( !isBulkCreate );
 
     return (
-        <Box className={`${ isNew ? "is-edit-student-id" : "is-edit-student" }`}>
-            <div className="is-flex" style={{ alignItems: "center" }}>
-                <StudenPanelHeading isNew={isNew} />
-                {student.elevation ? (
-                    <Tag
-                        color="danger"
-                        className="ml-2">
-                        <Icon icon="level-up-alt" />
-                    </Tag>
-                ) : null}
-                {isNew ? (
-                    <span className="ml-auto">
-                        <StudentOptions
-                            student={student}
-                            labelSize="small"
-                            className="is-right"
-                        />
-                    </span>
-                ) : (
-                    <span className="ml-auto">
-                        <Button
-                            size="small"
-                            onClick={toggleBulkCreate}
-                            color={
-                                isBulkCreate
-                                    ? "primary"
-                                    : null
-                            }>
-                            Bulk Add
-                        </Button>
-                    </span>
-                )}
-            </div>
-            <hr />
-            <SettingsForm
-                roomId={roomId}
-                student={student}
-                isBulkCreate={isBulkCreate}
-            />
-        </Box>
-    )
+		<Box className={`${isNew ? "is-edit-student" : "is-edit-student-id"}`}>
+			<div className="is-flex" style={{ alignItems: "center" }}>
+				<StudenPanelHeading isNew={isNew} />
+				{student.elevation ? (
+					<Tag color="danger" className="ml-2">
+						<Icon icon="level-up-alt" />
+					</Tag>
+				) : null}
+				{isNew ? (
+					<span className="ml-auto">
+						<StudentOptions
+							student={student}
+							labelSize="small"
+							className="is-right"
+						/>
+					</span>
+				) : (
+					<span className="ml-auto">
+						<Button
+							size="small"
+							onClick={toggleBulkCreate}
+							color={isBulkCreate ? "primary" : null}>
+							Bulk Add
+						</Button>
+					</span>
+				)}
+			</div>
+			<hr />
+			<SettingsForm
+				roomId={roomId}
+				student={student}
+				isBulkCreate={isBulkCreate}
+			/>
+		</Box>
+	);
 }
