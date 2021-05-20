061db462b48b07eac3498c8705eb3dec2ea728bc
daniel <danielsoledad@gmail.com>
2021-05-20 01:09:39 -0700

    modified:   client/src/App.js     added <Modal/> to App.js so it can be used in the entire application modified:   client/src/layouts/MainWithLogin.js     moved registration of login modal where is going to be used, imported all the components that it need to be render

--- client/src/layouts/MainWithLogin.js
+++ client/src/layouts/MainWithLogin.js
@@ -1,11 +1,27 @@
 import Main from "./Main";
-import { LoginModal } from "components/Login"
-
+import { LoginForm, LoginModal } from "components/Login"
+import { useModalRegistration } from "components/Modal/utils";
+import { Box, Heading } from "react-bulma-components";
+const modalKey = "LOGIN_MODAL"
 const MainWithLogin = ({ children }) => {
+
+    useModalRegistration(modalKey, {
+		key: modalKey,
+		component: () => (
+			<Box className="py-5">
+				<Heading renderAs="h2">Login</Heading>
+				<hr />
+				<LoginForm />
+				<p className="mt-3 has-text-grey is-size-7">
+					Don't have an account yet? <a href="/register">Register</a>
+				</p>
+			</Box>
+		)
+	});
+
     return (
         <div>
             <Main>{children}</Main>
-            <LoginModal />
         </div>
     )
 }
