4b98060d8f9d1471cabb26f701f80c6fb6786b4f
William Crownover <williamcrownover1@gmail.com>
2021-11-15 00:30:49 -0800

    Created login mutation which returns a token, success, and message using auth ctrls

--- app/config/graphql_schemas/resolvers.js
+++ app/config/graphql_schemas/resolvers.js
@@ -1,8 +1,23 @@
+const ctrls = require("../../controllers");
+
 const resolvers = {
 
     Query: {
         test: () => {
             return {success: true, message: 'Hello World.'}
+        }
+    },
+
+    Mutation: {
+        login: async ( parent, { credentials } ) => {
+            
+            const userLoggedIn = await ctrls.get("auth").login({credentials});
+            
+            return {
+                token: userLoggedIn.token,
+                success: true,
+                message: 'Logged In'
+            }
         }
     }
 }
