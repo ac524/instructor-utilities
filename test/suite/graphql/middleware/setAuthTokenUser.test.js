87dd5b97b51e50d8174ce6692690e8f8af27e470
Anthony Brown <ac524.brown@gmail.com>
2022-01-26 09:02:18 -0800

    First unit tests for setAuthTokenUser middleware.

---
+++ test/suite/graphql/middleware/setAuthTokenUser.test.js
@@ -0,0 +1,46 @@
+const { expect } = require("chai");
+const { stub } = require("sinon");
+
+const setAuthTokenUser = require("~crsm/graphql/middleware/setAuthTokenUser");
+
+const makeDb = () => {
+    const db = new Map();
+
+    db.set('user', {
+        findOne: stub().callsFake(() => 'userFindOneResult')
+    });
+
+    return db;
+}
+
+describe("setAuthTokenUser", () => {
+
+    it("should add `authUser` to `context` if provided a valid user id", async () => {
+
+      const context = {
+        authTokenData: { id: "AValidUserID" },
+        db: makeDb()
+      }
+
+      await setAuthTokenUser( { context }, () => {} );
+
+      // Assert - Expect a result.
+      expect( "authUser" in context ).to.equal( true );
+
+    });
+
+    it("should add assign the result of the user model `findOne` to `context.authUser`", async () => {
+
+        const context = {
+          authTokenData: { id: "AValidUserID" },
+          db: makeDb()
+        }
+  
+        await setAuthTokenUser( { context }, () => {} );
+  
+        // Assert - Expect a result.
+        expect( context.authUser ).to.equal( "userFindOneResult" );
+  
+      });
+
+});
